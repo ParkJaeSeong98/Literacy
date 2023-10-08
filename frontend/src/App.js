84de3cf43d4a71b112e93dde22def3c697513d57
ohyangho <swyangho@gmail.com>
2023-10-09 07:59:47 +0900

    edit App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,13 +15,13 @@ function App() {
     <BrowserRouter>
       <div>
         <div className="App">
-          <h2><Link to='/1'>기능1</Link></h2>
-          <h2><Link to='/2'>기능2</Link></h2>
+          <h2><Link to='/function1'>기능1</Link></h2>
+          <h2><Link to='/function2'>기능2</Link></h2>
           <br />
         </div>
         <Routes>
-          <Route path="/1" element={<Feature1></Feature1>} />
-          <Route path="/2" element={<Feature2></Feature2>} />
+          <Route path="/function1" element={<Feature1></Feature1>} />
+          <Route path="/function2" element={<Feature2></Feature2>} />
         </Routes>
       </div>
     </BrowserRouter>
