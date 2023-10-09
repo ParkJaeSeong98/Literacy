2fbca19bf0afc43156ffb80690a0db309b15bab4
ohyangho <swyangho@gmail.com>
2023-10-09 22:51:05 +0900

    2023-10-09 멘토링 때의 에러를 수정함.

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,11 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, '../Literacy-main/frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/', (req, res) => {
-    res.send('Here is root')
-})
-
-app.get('/function1', (req, res) => {
-    res.send('Function 1')
-})
-
-app.get('/function2', (req, res) => {
-    res.send('Function 2')
-})
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
