ad5236ea88b5d16c5d8dc815b7c4c95c4035035f
ohyangho <swyangho@gmail.com>
2023-10-11 23:37:14 +0900

    기능1에 단어 검색하면 우리말샘 사전의 1~100번째의 단어들이 출력되도록 함. api에서 단어 가져오는 건 해결했으니 이제 이 데이터들을 우리가 잘 요리하면 될듯.

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const fetch = require('node-fetch'); // npm install node-fetch
 var cors = require('cors');
 const path = require('path');
 const app = express();
 const port = 3000;
+
+const API_URL = 'http://opendict.korean.go.kr/api/search';
+const API_KEY = '3EA002DAFA58218A192AC80E387B8334'; // 우리말샘 오양호 발급키
 
 app.use(cors()); // 일단 모든 요청에 대해서 cors 허용 이 부분에서 조건 설정도 가능하다고 함.
 
 app.use(express.static(path.join(__dirname, '../frontend/build'))); // 리액트 기능1,기능2 앱 사용
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.get('/api/search', async (req, res) => {
+  const query = req.query.q;
+
+  try {
+    const response = await fetch(`${API_URL}?certkey_no=6027&key=${API_KEY}&target_type=search&req_type=json&part=word&q=${query}&sort=dict&start=1&num=100`); //start, end 조정가능
+    const data = await response.json();
+    res.json(data); // API 응답을 리액트 앱에 전달
+  } catch (error) { // 에러 처리
+    console.error('Error fetching from the dictionary API:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+  
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
-
-function Feature1() {
-  return <h2>기능1입니다</h2>;
-}
 
 const WordRelay = () => {
   const [word, setWord] = useState('시작');
@@ -42,6 +38,64 @@ const WordRelay = () => {
   );
 };
 
+function KoreanDictionary() {
+  const [tempInput, setTempInput] = useState(''); // 임시로 입력값을 저장할 상태
+  const [storedValue, setStoreValue] = useState(''); // 버튼 클릭 시 값을 저장할 상태
+  const [words, setWords] = useState([]); // API 응답에서 가져온 단어들을 저장할 상태
+
+  const handleInputChange = (event) => {
+    setTempInput(event.target.value); // 입력값 변화 감지 시 임시 상태에 저장
+  };
+
+  const handleButtonClick = () => {
+    setStoreValue(tempInput); // 버튼 클릭 시 임시 상태의 값을 storedValue에 저장
+    getJsonFromDictionaryAPI(tempInput);
+  };
+
+  const getJsonFromDictionaryAPI = (query) => { // 우리 백엔드 api로 연결함. 프론트에서 직접 요청하면 CORS 오류 때문에 직접 할 수가 없음.
+    fetch(`http://localhost:3000/api/search?q=${query}`) // 백엔드에서 호출
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(query);
+      console.log(data);
+      const fetchedWords = data.channel.item.map(item => item.word);
+      setWords(fetchedWords); // API 응답에서 가져온 단어들을 상태에 저장
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  };
+
+  return (
+    <div>
+      <p>설명: 우리 backend/index.js 에서 우리말샘 사전 api로 요청을 보낸 후에 받은 json을<br/> 
+        다시 frontend/App.js 에서 backend/index.js의 api를 통해 받아가도록 구성했음.<br/>
+        검색해서 나오는 단어 목록들은 입력창에 입력한 단어를 우리말샘 사전에 검색했을 때<br/>
+        결과로 나오는 단어 첫 번째부터 백 번째까지 단어의 설명을 빼고 나열한 것임.<br/>
+        </p>
+      <input
+        type="text"
+        value={tempInput}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>검색</button>
+      <div>
+      <h1>검색하신 단어는 "{storedValue}" 입니다.</h1>
+        {
+          words.map(function(content){ // words 리스트 쭉 나열하기
+            return (
+              <div>
+                {content}
+                <hr/>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
@@ -52,7 +106,7 @@ function App() {
           <br />
         </div>
         <Routes>
-          <Route path="/function1" element={<Feature1></Feature1>} />
+          <Route path="/function1" element={<KoreanDictionary></KoreanDictionary>} />
           <Route path="/function2" element={<WordRelay></WordRelay>} />
         </Routes>
       </div>
